8cc022e5a088f3fcae22bfec28d4ce7eb3ef287e
henry cheche <henry.cheche@student.moringaschool.com>
2023-03-14 14:49:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+// Define the saturdayFun function
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+  }
+
+  wrapAdjective("%")("a dedicated programmer");
+  // Define the wrapAdjective function
+function wrapAdjective(string = "*") {
+    return function(adjective = "special") {
+      return `You are ${string}${adjective}${string}!`;
+    }
+  }
